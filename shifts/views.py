988f25529ac9e3e6a14dc930330b73cb97abae5e
Susie Gordon <susieyoungchoi@gmail.com>
2023-09-20 19:32:13 -0400

    created viewset

--- shifts/views.py
+++ shifts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from .models import Shift
+from rest_framework import viewsets, permissions
+from .serializers import ShiftSerializer
 
 # Create your views here.
+
+class ShiftViewSet(viewsets.ModelViewSet):
+    # queryset is a list of all Shift objects
+    queryset = Shift.objects.all()
+    # serializer_class attribute is used to control which serializer class should be used for serializing & deserializing queryset & model instances
+    serializer_class = ShiftSerializer
+    # set permission_classes to allow unrestricted access to the api
+    permission_classes = [permissions.AllowAny]
