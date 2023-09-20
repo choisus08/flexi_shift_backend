45f99ff3b675028cdeb9658349450f31ca5f0e3c
Susie Gordon <susieyoungchoi@gmail.com>
2023-09-20 19:27:38 -0400

    corrected Shift model code

--- shifts/models.py
+++ shifts/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Shift(models.Model):
-    name = models.Charfield(max_length=200)
-    position = models.Charfield(max_length=200)
-    date = models.Charfield(max_length=200)
-    start_time = models.Charfield(max_length=200)
-    end_time = models.Charfield(max_length=200)
+    name = models.CharField(max_length=200)
+    position = models.CharField(max_length=200)
+    date = models.CharField(max_length=200)
+    start_time = models.CharField(max_length=200)
+    end_time = models.CharField(max_length=200)
