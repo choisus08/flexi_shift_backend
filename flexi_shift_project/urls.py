1cf73d8e615bc3687e63fb9a5beff0fde4c95ba6
Susie Gordon <susieyoungchoi@gmail.com>
2023-09-20 19:43:48 -0400

    set up router & tested routes

--- flexi_shift_project/urls.py
+++ flexi_shift_project/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from shifts.views import ShiftViewSet
+
+## Create a new router
+router = routers.DefaultRouter()
+## Register our viewsets
+router.register(r'shifts', ShiftViewSet)
 
 urlpatterns = [
+    # add all ofo ur router urls
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
